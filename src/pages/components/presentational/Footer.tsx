4ff38cc168876a86a6e07ea3d31f6fa98e92e918
tkawakita <tommycsth61@gmail.com>
2021-03-01 01:30:56 +0900

    Footerコンポーネントを追加

---
+++ src/pages/components/presentational/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const Footer: FC = () => (
+  <footer>
+    <div>
+      このサイトではアクセス解析のためにcookieを使用したGoogle
+      Analyticsを使用しています
+    </div>
+    <div>@2021 Tommy All Rights Reserved.</div>
+  </footer>
+);
+
+export default Footer;
