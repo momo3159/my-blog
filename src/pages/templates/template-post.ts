e35bd9eca07a27ea8d3e38708ced160e88a592e0
tkawakita <tommycsth61@gmail.com>
2021-03-01 21:48:17 +0900

    access tokenを削除

---
+++ src/pages/templates/template-post.ts
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import Header from '../components/presentational/Header';
+import Footer from '../components/presentational/Footer';
+
+const Post: FC = () => {
+  return (
+    <div>hoge</div>
+  )
+}
+
+export default Post
