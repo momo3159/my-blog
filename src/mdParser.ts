590cf68f338832351cd8a25814355f9c7a07102e
tkawakita <tommycsth61@gmail.com>
2021-08-06 01:55:51 +0900

    highlightの修正

--- src/mdParser.ts
+++ src/mdParser.ts
@@ -3,6 +3,7 @@ import highlightjs from 'highlight.js';
 
 marked.setOptions({
   highlight(code, lang) {
+    const language = highlightjs.getLanguage(lang) ? lang : 'plaintext'
     return highlightjs.highlightAuto(code, [lang]).value;
   },
   pedantic: false,
